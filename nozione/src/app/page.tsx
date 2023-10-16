a117b996fdac7dd28fec95174c6b406170633f05
arther-nux <arther@memomonge.com>
2023-10-16 16:41:29 -0700

    Added first shadcn-ui button element

--- nozione/src/app/page.tsx
+++ nozione/src/app/page.tsx
@@ -1,7 +1,6 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <h1 className='text-blue-600'>Hello World!</h1>
-  )
+  return <Button>Click Me!</Button>;
 }
